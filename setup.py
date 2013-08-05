9b3d918269a1d632c80c18e1f65055c9171b4e1b
Anton de Winter <adewinter@gmail.com>
2013-08-04 21:52:29 -0400

    modded setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 
 setup(
     name='aamnotifs',
-    version='0.1.0',
+    version='0.1.1',
     author='Andrei Marcu',
     author_email='andrei@marcu.net',
     packages=['aamnotifs'],
-    url='http://github.com/andreimarcu/aamnotifs',
+    url='http://github.com/adewinter/aamnotifs',
     license='LICENSE.txt',
     description='Simple notifications implementation with RabbitMQ using pika.',
     install_requires=[
